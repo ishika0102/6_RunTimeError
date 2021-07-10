fc0b405d0cde12831cb3dbdb5a9f757a93d730d6
Shivang Yadav <shivang1691401@gmail.com>
2021-07-10 23:53:55 +0530

    converted to react

---
+++ change html to react/profile_Supp/ProfileSupplier.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import '../css/main.css'
+
+function profilev() {
+  return (
+    <div className="App">
+    <div className="v93_538"><div className="v93_541" /><div className="v93_580"><div className="v93_591" /><div className="v93_592"><span className="v93_593">Contact</span><span className="v93_594">+91 9678123567</span></div><div className="v93_595"><span className="v93_596">Region</span><span className="v93_597">Hosur</span></div><div className="v93_598"><span className="v93_599">Company Assosciated</span><span className="v93_600">Parle</span></div><div className="v93_601"><span className="v93_602">Name</span><span className="v93_603">Fabian Levy</span></div><div className="v93_626" /></div><div className="v93_1302"><span className="v93_1303">E-mail Address</span><span className="v93_1304">abc@gmail.com</span></div><div className="v93_573"><div className="v93_574"><span className="v93_575">Profile</span><span className="v93_576">Fabian's lifestyle is quite active. He works out three times a week. His fitness routine is a healthy mix of strength-training and cardio. Apart from fitness, his priority is a healthy and balanced diet. He have some sort of a meal plan, but he is quite fliexible with it.
+              He prefers to do shopping online because it is convenient and less time-consuming, as he can do it whenever it suits him.</span></div></div><div className="v93_540"><div className="name" /><div className="v107_757" /></div><div className="v93_1300"><div className="v93_1291" /><div className="v96_1015" /><div className="v96_1016" /><div className="v93_1292" /><div className="v93_1297" /><div className="v93_1298" /><div className="v93_1299" /><span className="v93_1293">LOCAL GROCERY STORES</span><span className="v93_1294">PRODUCTS IN WAREHOUSE</span><span className="v93_1295">PRODUCTS AVAILABILITY</span><span className="v93_1296">NEW LAUNCHES</span></div><div className="v96_1014" /><div className="v93_1499" /></div>
+    </div>
+  );
+}
+
+export default profilev;
